34f3d34f64bf79a48c5f8e918e26708b1fc8a85f
Gustavo Sales <gustavosalessantos@gmail.com>
2020-03-10 10:16:55 -0300

    fix: month decrement

--- src/common-components/month-chooser-field/index.tsx
+++ src/common-components/month-chooser-field/index.tsx
@@ -3,86 +3,90 @@ import arrow from '../../assets/svg/icons/arrow.svg';
 import getMonthsFrom from '../../lib/get-months-from';
 
 import {
-  StyledWrapper,
-  StyledLabel,
-  StyledDatePickerField,
-  StyledArrow
+	StyledWrapper,
+	StyledLabel,
+	StyledDatePickerField,
+	StyledArrow
 } from './styles';
 import Svg from '../svg';
 
 export enum MonthChooserArrowTypes {
-  INCREMENT,
-  DECREMENT
+	INCREMENT,
+	DECREMENT
 }
 
 interface MonthChooserFieldProps {
-  label: React.ReactNode;
-  defaultValue: number;
-  onChange: (...args: any[]) => any;
-  children?: React.ReactNode;
-  id?: string;
+	label: React.ReactNode;
+	defaultValue: number;
+	onChange: (...args: any[]) => any;
+	children?: React.ReactNode;
+	id?: string;
 }
 
 const MonthChooserField: React.SFC<MonthChooserFieldProps> = ({
-  label,
-  defaultValue,
-  onChange,
-  ...props
+	label,
+	defaultValue,
+	onChange,
+	...props
 }) => {
-  const currentDate = new Date();
-  const [months, setMonths] = React.useState(defaultValue);
-  const [dateLabel, setDateLabel] = React.useState(
-    getMonthsFrom(currentDate, defaultValue)
-  );
+	const currentDate = new Date();
+	const [months, setMonths] = React.useState(defaultValue);
+	const [dateLabel, setDateLabel] = React.useState(
+		getMonthsFrom(currentDate, defaultValue)
+	);
 
-  function handleChangeMonths(type: MonthChooserArrowTypes) {
-    let newMonthValue = months;
+	function handleChangeMonths(type: MonthChooserArrowTypes) {
+		let newMonthValue = months;
 
-    if (type === MonthChooserArrowTypes.DECREMENT && months > 2) {
-      newMonthValue = months - 1;
-    }
+		if (type === MonthChooserArrowTypes.DECREMENT && months > 1) {
+			newMonthValue = months - 1;
+		}
 
-    if (type === MonthChooserArrowTypes.INCREMENT) {
-      newMonthValue = months + 1;
-    }
+		if (type === MonthChooserArrowTypes.INCREMENT) {
+			newMonthValue = months + 1;
+		}
 
-    const dateLabels = getMonthsFrom(currentDate, newMonthValue);
+		const dateLabels = getMonthsFrom(currentDate, newMonthValue);
 
-    setMonths(newMonthValue);
-    setDateLabel(dateLabels);
+		setMonths(newMonthValue);
+		setDateLabel(dateLabels);
 
-    onChange(newMonthValue, dateLabels);
-  }
+		onChange(newMonthValue, dateLabels);
+	}
 
-  return (
-    <div>
-      <StyledLabel>{label}</StyledLabel>
-      <StyledWrapper {...props}>
-        <StyledArrow
-          className="prev-arrow"
-          onClick={() => handleChangeMonths(MonthChooserArrowTypes.DECREMENT)}
-        >
-          <Svg src={arrow} />
-        </StyledArrow>
-        <StyledDatePickerField className="date">
-          <strong>{dateLabel.month}</strong>
-          <span>{dateLabel.year}</span>
-        </StyledDatePickerField>
-        <StyledArrow
-          next
-          className="next-arrow"
-          onClick={() => handleChangeMonths(MonthChooserArrowTypes.INCREMENT)}
-        >
-          <Svg src={arrow} />
-        </StyledArrow>
-      </StyledWrapper>
-    </div>
-  );
+	return (
+		<div>
+			<StyledLabel>{label}</StyledLabel>
+			<StyledWrapper {...props}>
+				<StyledArrow
+					className="prev-arrow"
+					onClick={() =>
+						handleChangeMonths(MonthChooserArrowTypes.DECREMENT)
+					}
+				>
+					<Svg src={arrow} />
+				</StyledArrow>
+				<StyledDatePickerField className="date">
+					<strong>{dateLabel.month}</strong>
+					<span>{dateLabel.year}</span>
+				</StyledDatePickerField>
+				<StyledArrow
+					next
+					className="next-arrow"
+					onClick={() =>
+						handleChangeMonths(MonthChooserArrowTypes.INCREMENT)
+					}
+				>
+					<Svg src={arrow} />
+				</StyledArrow>
+			</StyledWrapper>
+		</div>
+	);
 };
 
 MonthChooserField.defaultProps = {
-  onChange: () => {},
-  defaultValue: 1
+	onChange: () => {},
+	defaultValue: 1
 };
 
 export default MonthChooserField;
