fe5cc5aec2f36e534795d8fa5dea3c28e35648f6
Gustavo Sales <gustavosalessantos@gmail.com>
2020-03-09 23:01:13 -0300

    fix: small style fixes

--- src/common-components/cardboard/index.tsx
+++ src/common-components/cardboard/index.tsx
@@ -4,36 +4,36 @@ import devices from '../../configs/styles/devices';
 import Card from '../card';
 
 interface CardboardProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 const Cardboard: React.SFC<CardboardProps> = ({ children }) => {
-  const StyledWrapper = styled.div`
-    animation: fadeIn 800ms ease-out;
+	const StyledWrapper = styled.div`
+		animation: fadeIn 800ms ease-out;
 
-    @media ${devices.desktop} {
-      width: 56rem;
-      margin: 0 auto;
-    }
+		@media ${devices.desktop} {
+			width: 56rem;
+			margin: 0 auto 5rem;
+		}
 
-    @keyframes fadeIn {
-      from {
-        transform: translateY(20px);
-        opacity: 0;
-        visibility: hidden;
-      }
-      to {
-        transform: translateY(0);
-        opacity: 1;
-        visibility: visible;
-      }
-    }
-  `;
+		@keyframes fadeIn {
+			from {
+				transform: translateY(20px);
+				opacity: 0;
+				visibility: hidden;
+			}
+			to {
+				transform: translateY(0);
+				opacity: 1;
+				visibility: visible;
+			}
+		}
+	`;
 
-  return (
-    <StyledWrapper>
-      <Card layout>{children}</Card>
-    </StyledWrapper>
-  );
+	return (
+		<StyledWrapper>
+			<Card layout>{children}</Card>
+		</StyledWrapper>
+	);
 };
 
 export default Cardboard;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,32 +5,33 @@ import originLogo from '../../assets/svg/icons/logo.svg';
 import devices from '../../configs/styles/devices';
 
 const Header: React.SFC = () => {
-  const StyledHeader = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: #fff;
-    height: 5.6rem;
-    padding: 0;
-    margin-bottom: 3rem;
-    position: fixed;
-    width: 100%;
-    top: 0;
-    z-index: 1;
+	const StyledHeader = styled.div`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: #fff;
+		height: 5.6rem;
+		padding: 0;
+		margin-bottom: 3rem;
+		position: fixed;
+		width: 100%;
+		top: 0;
+		z-index: 1;
 
-    @media ${devices.desktop} {
-      position: relative;
-      justify-content: flex-start;
-      padding: 0 4rem;
-      margin-bottom: 4.5rem;
-    }
-  `;
+		@media ${devices.desktop} {
+			position: relative;
+			justify-content: flex-start;
+			padding: 0 4rem;
+			margin-bottom: 4.5rem;
+			box-sizing: border-box;
+		}
+	`;
 
-  return (
-    <StyledHeader>
-      <Svg src={originLogo} width="7rem" height="2.3rem" />
-    </StyledHeader>
-  );
+	return (
+		<StyledHeader>
+			<Svg src={originLogo} width="7rem" height="2.3rem" />
+		</StyledHeader>
+	);
 };
 
 export default Header;
